832250a43823b0e5668f7f5bbed8b09adca07c0e
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2025-07-07 17:49:59 -0300

    seventh commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import Main_Page from './components/main_page'
 import NavBar from './components/navbar'
 import About_Me from './components/secundary/AboutMe'
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <NavBar/>
-    <Main_Page/>
-    <About_Me/>
+    <Parallax pages={2}>
+      <ParallaxLayer 
+        speed={0.8}
+        factor={1.2}
+        style={{
+          backgroundSize: '75%'
+        }}>
+        <NavBar/>
+        <Main_Page />
+      </ParallaxLayer>
+      <ParallaxLayer 
+        offset={1}
+        speed={0.5}
+        factor={2.2}
+        style={{
+          backgroundSize: '75%'
+        }}
+        >
+        <About_Me />
+      </ParallaxLayer>
+    </Parallax>
     </>
   )
 }
