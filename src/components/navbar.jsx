4ee3d7e453b4ec4e494991b2485198d7f6b0ddcb
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2025-07-28 19:58:59 -0300

    thirteenth commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,26 @@
 import { useState, useEffect } from 'react';
 import './navbar.css';
+import Projects from './secundary/projects/Projects';
 
 function NavBar() {
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [isMenu, setMenu] = useState(false)
 
+    const towardPartOfWebsite = (e, name)=>{
+        e.preventDefault()
+        const menu = document.querySelector('.container')
+        const aboutme = document.querySelector('.container_about')
+        const projects = document.querySelector('.container_projects')
+        if(name === 'menu'){
+            menu.scrollIntoView({behavior: 'smooth'})
+        }
+        if(name === 'aboutme'){
+            aboutme.scrollIntoView({behavior: 'smooth'})
+        }
+        if(name === 'projects'){
+            projects.scrollIntoView({behavior: 'smooth'})
+        }
+    }
     useEffect(() => {
         document.body.classList.toggle('dark-mode', isDarkMode);
     });
@@ -16,9 +32,9 @@ function NavBar() {
             <div className={`navbar_background ${!isMenu ? '' : 'active'}`}>
                 <div className="navbar_close">
                     <div className="navbar">
-                        <a href="#">Home</a>
-                        <a href="#">About Me</a>
-                        <a href="#">Projects</a>
+                        <a href="#" onClick={(e)=>towardPartOfWebsite(e, 'menu')}>Home</a>
+                        <a href="#" onClick={(e)=>towardPartOfWebsite(e, 'aboutme')}>About Me</a>
+                        <a href="#" onClick={(e)=>towardPartOfWebsite(e, 'projects')}>Projects</a>
                         <a href="https://www.instagram.com/tuttadocs/">Follow me</a>
                     </div>
                     <div className="close" onClick={()=> setMenu(!isMenu)}><i class="ri-close-line"></i></div>
